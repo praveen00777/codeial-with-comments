ca4d20837b2472fe4c43c0ece1ab271a67bba4f5
sasapu praveen kumar <sasapupraveen@gmail.com>
2023-07-07 12:36:53 +0530

    added all important layouts and connected to database for our codial app

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/codeial');
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console, "error connecting to mongodb"));
+
+db.once('open',function(){
+      console.log("connected to database :: mongodb");
+
+});
+
+module.exports=db;
